24b42355519ce91cb6553b8c0984b6e2fd5c56b1
Roshan <roshanair1620@gmail.com>
2018-03-27 05:41:30 +0000

    Editing, with face detect demo

--- raspi.py
+++ raspi.py
@@ -47,7 +47,8 @@ def videofeed_on():
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
         # show the frame
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5) 
+        print len(faces)
         for (x,y,w,h) in faces:
             cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
             roi_gray = gray[y:y+h, x:x+w]
@@ -57,8 +58,8 @@ def videofeed_on():
                 cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
         
         cv2.imshow("Frame", image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        #cv2.waitKey(0)
+        #cv2.destroyAllWindows()
         key = cv2.waitKey(1) & 0xFF
 
         # clear the stream in preparation for the next frame
@@ -78,8 +79,9 @@ def take_picture():
     # grab an image from the camera
     camera.capture(rawCapture, format="bgr")
     if output:
+        print "Picture saved"
         camera.capture(output)
-        image = rawCapture.array
+    image = rawCapture.array
 
     # display the image on screen and wait for a keypress
     cv2.imshow("Image", image)
